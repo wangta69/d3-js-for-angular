0834e0beda0fb4e6f3e3688622d1f1b774bcfe5a
younghyeong ryu <wangta69@naver.com>
2021-01-12 17:09:08 +0900

    t
    t

--- src/components/line1-component.ts
+++ src/components/line1-component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, HostListener, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
-import { DecimalPipe } from '@angular/common';
+// import { DecimalPipe } from '@angular/common';
 import * as d3 from 'd3';
-
 
 export interface ChartData {
     date: any;
@@ -51,43 +50,67 @@ export class LineChartComponent implements OnInit {
         this.createData();
         this.element = this.chartContainer.nativeElement;
         this.svg = d3.select(this.element).append('svg');
+
+        const body = this.svg.append('g');
+        const gEnter = this.svg.selectAll(); // data([data]).enter().append('svg').append('g');
+        body.append('g').attr('class', 'axis x');
+        body.append('g').attr('class', 'axis y');
+        body.append('defs').append('clipPath')
+            .attr('id', 'clip')
+          .append('rect')
+            .attr('width', this.width - this.margin.left - this.margin.right)
+            .attr('height', this.height - this.margin.top - this.margin.bottom);
+
+        body.append('g')
+            .attr('class', 'lines')
+            .attr('clip-path', 'url(#clip)')
+    //      .selectAll('.data').data(data).enter()
+            .append('path')
+            .attr('class', 'data');
+
+        const legendEnter = body.append('g')
+          .attr('class', 'legend')
+          .attr('transform', 'translate(' + (this.width - this.margin.right - this.margin.left - 75) + ', 25)');
+        legendEnter.append('rect')
+          .attr('width', 50)
+          .attr('height', 75)
+          .attr('fill', '#ffffff')
+          .attr('fill-opacity', 0.7);
     }
 
 
     // 임시 데이타 만들기 시작
-    private createData() {
+    private createData(): void  {
         this.seedData();
         setInterval(() => {
             this.updateData();
         }, 500);
-
-
     }
 
-    private randomNumberBounds(min, max) {
+    private randomNumberBounds(min: number, max: number): void {
       return Math.floor(Math.random() * max) + min;
     }
 
-    private seedData() {
+    private seedData(): void {
       const now = new Date();
       for (let i = 0; i < this.MAX_LENGTH; ++i) {
         this.lineArr.push({
           time: new Date(now.getTime() - ((this.MAX_LENGTH - i) * this.duration)),
           x: this.randomNumberBounds(0, 5),
           y: this.randomNumberBounds(0, 2.5),
-          z: this.randomNumberBounds(0, 10)
+          z: this.randomNumberBounds(0, 10),
         });
       }
     }
 
-    private updateData() {
+    private updateData(): void {
       const now = new Date();
 
       const lineData = {
         time: now,
         x: this.randomNumberBounds(0, 5),
         y: this.randomNumberBounds(0, 2.5),
-        z: this.randomNumberBounds(0, 10)
+        z: this.randomNumberBounds(0, 10),
       };
       this.lineArr.push(lineData);
 
@@ -111,7 +134,7 @@ export class LineChartComponent implements OnInit {
     // 임시 데이타 만들기 끝
 
     // 차트 그리기
-    private draw() {
+    private draw(): void {
         const data = ['x', 'y', 'z'].map((c) => {
                 return {
                   label: c,
@@ -121,111 +144,84 @@ export class LineChartComponent implements OnInit {
                 };
               });
 
-
-        console.log(data);
+    //    console.log(data);
         const t = d3.transition().duration(this.duration).ease(d3.easeLinear);
         const x = d3.scaleTime().rangeRound([0, this.width - this.margin.left - this.margin.right]);
         const y = d3.scaleLinear().rangeRound([this.height - this.margin.top - this.margin.bottom, 0]);
         const z = d3.scaleOrdinal(this.color);
     //
-        const xMin = d3.min(data, function(c) { return d3.min(c.values, function(d) { return d.time; })});
-        const xMax = new Date(new Date(d3.max(data, function(c) {
-        return d3.max(c.values, function(d) { return d.time; })
-        })).getTime() - (this.duration * 2));
+        const xMin = d3.min(data, (c) => d3.min(c.values, (d) => d.time));
+        const xMax = new Date(new Date(d3.max(data, (c) => d3.max(c.values, (d) => d.time))).getTime() - (this.duration * 2));
     //
         x.domain([xMin, xMax]);
         y.domain([
-        d3.min(data, function(c) { return d3.min(c.values, function(d) { return d.value; })}),
-        d3.max(data, function(c) { return d3.max(c.values, function(d) { return d.value; })})
+            d3.min(data, (c) => d3.min(c.values, (d) => d.value)),
+            d3.max(data, (c) => d3.max(c.values, (d) => d.value )),
         ]);
-        z.domain(data.map(function(c) { return c.label; }));
+        z.domain(data.map((c) => c.label));
         //
         const line = d3.line()
-        .curve(d3.curveBasis)
-        .x(function(d) { return x(d.time); })
-        .y(function(d) { return y(d.value); });
+            .curve(d3.curveBasis)
+            .x((d) => x(d.time))
+            .y((d) => y(d.value));
     //
     //   var svg = d3.select(this).selectAll('svg').data([data]);
         // const svg = d3.select(this.element).append('svg').data([data]);
-     const svg = this.svg;
-      const gEnter = this.svg.data([data]).enter().append('svg').append('g');
-      gEnter.append('g').attr('class', 'axis x');
-      gEnter.append('g').attr('class', 'axis y');
-      gEnter.append('defs').append('clipPath')
-          .attr('id', 'clip')
-        .append('rect')
-          .attr('width', this.width-this.margin.left-this.margin.right)
-          .attr('height', this.height-this.margin.top-this.margin.bottom);
 
-      gEnter.append('g')
-          .attr('class', 'lines')
-          .attr('clip-path', 'url(#clip)')
-        .selectAll('.data').data(data).enter()
-          .append('path')
-            .attr('class', 'data');
-
-      const legendEnter = gEnter.append('g')
-        .attr('class', 'legend')
-        .attr('transform', 'translate(' + (this.width-this.margin.right-this.margin.left-75) + ',25)');
-      legendEnter.append('rect')
-        .attr('width', 50)
-        .attr('height', 75)
-        .attr('fill', '#ffffff')
-        .attr('fill-opacity', 0.7);
-      legendEnter.selectAll('text')
-        .data(data).enter()
-        .append('text')
-          .attr('y', function(d, i) { return (i*20) + 25; })
-          .attr('x', 5)
-          .attr('fill', function(d) { return z(d.label); });
+        const legendEnter = this.svg.select('.legend');
+        legendEnter.selectAll('text')
+            .data(data).enter()
+            .append('text')
+            .attr('y', (d, i: number) => (i * 20) + 25)
+            .attr('x', 5)
+            .attr('fill', (d) => z(d.label));
 
     //   var svg = selection.select('svg');
-      svg.attr('width', this.width).attr('height', this.height);
-      const g = svg.select('g')
-        .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+        this.svg.attr('width', this.width).attr('height', this.height);
+        const g = this.svg.select('g')
+            .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
 
-      g.select('g.axis.x')
-        .attr('transform', 'translate(0,' + (this.height-this.margin.bottom-this.margin.top) + ')')
-        .transition(t)
-        .call(d3.axisBottom(x).ticks(5));
-      g.select('g.axis.y')
-        .transition(t)
-        .attr('class', 'axis y')
-        .call(d3.axisLeft(y));
+        this.svg.select('g.axis.x')
+            .attr('transform', 'translate(0,' + (this.height - this.margin.bottom - this.margin.top) + ')')
+            .transition(t)
+            .call(d3.axisBottom(x).ticks(5));
 
-      g.select('defs clipPath rect')
-        .transition(t)
-        .attr('width', this.width-this.margin.left-this.margin.right)
-        .attr('height', this.height-this.margin.top-this.margin.right);
+        g.select('g.axis.y')
+            .transition(t)
+            .call(d3.axisLeft(y));
 
-      g.selectAll('g path.data')
-        .data(data)
-        .style('stroke', function(d) { return z(d.label); })
-        .style('stroke-width', 1)
-        .style('fill', 'none')
-        .transition()
-        .duration(this.duration)
-        .ease(d3.easeLinear)
-        .on('start', tick);
+        g.select('defs clipPath rect')
+            .transition(t)
+            .attr('width', this.width - this.margin.left - this.margin.right)
+            .attr('height', this.height - this.margin.top - this.margin.right);
 
-      g.selectAll('g .legend text')
-        .data(data)
-        .text(function(d) {
-          return d.label.toUpperCase() + ': ' + d.values[d.values.length-1].value;
-        });
+        g.selectAll('g path.data')
+            .data(data)
+            .style('stroke', (d) => z(d.label))
+            .style('stroke-width', 1)
+            .style('fill', 'none')
+            .transition()
+            .duration(this.duration)
+            .ease(d3.easeLinear)
+            .on('start', tick);
+
+        g.selectAll('g .legend text')
+            .data(data)
+            .text((d) => d.label.toUpperCase() + ': ' + d.values[d.values.length - 1].value);
 
     //   // For transitions https://bl.ocks.org/mbostock/1642874
-     function tick() {
-    //     d3.select(this)
-    //       .attr('d', function(d) { return line(d.values); })
-    //       .attr('transform', null);
-    //
-    //     var xMinLess = new Date(new Date(xMin).getTime() - this.duration);
-    //     d3.active(this)
-    //         .attr('transform', 'translate(' + x(xMinLess) + ',0)')
-    //       .transition()
-    //         .on('start', tick);
-       }
+        function tick(m: any, i: number, n: []): void {
+            d3.select(n[i])
+            .attr('d', (d) => line(d.values))
+            .attr('transform', null);
+        //    console.log(this.duration);
+            const xMinLess = new Date(new Date(xMin).getTime() - this.duration);
+        //    console.log(xMinLess);
+        d3.active(n[i])
+            .attr('transform', 'translate(' + x(xMinLess) + ',0)')
+            .transition()
+            .on('start', tick);
+        }
     // });
     }
     //
